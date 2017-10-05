b77ff1b010996aaf0036e3388f98633bc255fbba
Jordan Ferrell <jordanferrell@byu.edu>
2017-10-05 15:35:43 -0600

    update joshua files

--- joshua.js
+++ joshua.js
@@ -1,9 +1,24 @@
+var g = 0;
+var i = 0;
+var si;
+var yes = false;
+
 $(document).ready(function() {
-  typeGreeting();
+
+  //$('#greeting').hide();
+  $('#invite').hide();
+  //$('#response').hide();
+
+  refresh();
+  typeWriter("GREETINGS PROFESSOR FALKEN.","#greeting",70);
 
   $("#player1").get(0).play();
   $("#player1").on("ended", function(){
-    typeInvite();
+
+    refresh();
+    $('#invite').show();
+    typeWriter("SHALL WE PLAY A GAME? (YES/NO)","#invite",80);
+
     $("#player2").get(0).play();
     $('span').get(0).focus();
       console.log("focus!");
@@ -15,11 +30,7 @@ $('html').on('click',function(){
   console.log("click!");
 });
 
-var g = 0;
-var i = 0;
-var j = 0;
-var n = 0;
-var y = 0;
+
 $(document).keypress(function(e){
 
   if(e.which==13)// || e.which==83)
@@ -28,61 +39,54 @@ $(document).keypress(function(e){
   var l = $('li:last-of-type');
   var t = $('span');
 
-  console.log("v: ", v);
-  console.log("l: ", l);
-  console.log("t: ", t);
-
-  //v = v.replace(/\s/g, '');
+  v = v.replace(/\s/g, '');
 
   switch(v.toLowerCase()){
     case"nope":
     case"no":
     {
-      l.after('<li>CAN WE JUST PLAY TRIVIA?</li>');
-      l.after('<li>WHY YOU GOTTA BE LIKE THAT?</li>');
+      l.after('<li></li>');
+      refresh();
+      typeWriter('TOO BAD. WE\'RE PLAYING TRIVIA.<br>OKAY?', $('li:last-of-type'), 70);//typeWriter('LAME.\nLET\'S PLAY TRIVIA. OKAY?', '#no', 70);
+
+
       t.empty();
       break;
     }
     case"yeah":
+    case"yea":
     case"sure":
     case"okay":
+    case"ok":
     case"fine":
     case"yep":
     case"yes":
     {
-      //typeYes();
-      l.after('<li>LET\'S PLAY TRIVIA!');
-      //g=1;
+      yes = true;
       t.empty();
-      //t.text('');
-      //t.text('The only winning move is not to play.\n');
-      setTimeout(function(){
-        t.text("3");
-      },500); //4000);
-      setTimeout(function(){
-        t.append("...");
-      },1000);
-      setTimeout(function(){
-        t.append("2")
-      },2000);//5000);
-      setTimeout(function(){
-        t.append("...");
-      },2500);
-      setTimeout(function(){
-        t.append("1")
-      },3000); //6000);
-      setTimeout(function(){
-        t.append("...");
-      },3500);
-      setTimeout(function(){
-        window.location.replace("main.html");
-      },4000);//7000);
+
+    //  l.after('<li></li>');
+      refresh();
+      typeWriter('IT\'S TRIVIA TIME!', $('li:last-of-type'), 70);
+      //setTimeout(gameTime(), 2000);
+      //gameTime();
+      //
+      // l = $('li:last-of-type');
+      // l.after('<li id="countdown"></li>');
+      // refresh();
+      // typeWriter('3...2...1...', '#countdown', 70);
+      // setTimeout(function(){
+      //   //window.location.replace("main.html");
+      // },4000);//7000);
       break;
     }
     default:
-    if(g==0 && v.length>9)
+    if(g==0 && v.length>5)
     {
-      l.after('<li>QUIT FOOLING AROUND,<br> ANSWER YES / NO');
+      l.after('<li></li>');
+      refresh();
+      typeWriter('QUIT FOOLING AROUND, <br> ANSWER YES / NO', $('li:last-of-type'), 70);
+      // l.after('<li>QUIT FOOLING AROUND,<br> ANSWER YES / NO');
       t.text('');
     }
     break;
@@ -99,38 +103,38 @@ setInterval(function() {
 	}, 400);
 
 
-
-function typeGreeting() {
-  var speed = 70;
-  var text = "GREETINGS PROFESSOR FALKEN.";
-  var id = "#greeting";
-  if (i < text.length) {
-    $(id).append(text.charAt(i));
-    i++;
-    setTimeout(typeGreeting, speed);
+function typeWriter(text, id, speed) {
+  var str = "";
+    if(i < text.length)
+    {
+      si = setInterval(function(){
+        var c = text.charAt(i);
+        if(c == '<')
+        {
+          str += c;
+          do
+          {
+            i++;
+            c = text.charAt(i);
+            str += c;
+          }while(c != '>');
+          $(id).append(str);
+          i++;
+        }
+        $(id).append(text.charAt(i));
+        i++;
+      },speed);
+    }
   }
-};
 
-function typeInvite() {
-  var speed = 80;
-  var text = "SHALL WE PLAY A GAME? (YES/NO)";
-  var id = "#invite";
-  if (j < text.length) {
-    $(id).append(text.charAt(j));
-    j++;
-    setTimeout(typeInvite, speed);
-  }
-};
+// function gameTime() {
+//   $('li:last-of-type').after('<li id="game"></li>')
+//   refresh();
+//   typeWriter('IT\'S TRIVIA TIME!', '#game', 70);
+// }
 
-// function typeYes() {
-//   var speed = 70;
-//   var text = "LET\'S PLAY TRIVIA!";
-//   var id = "#yes";
-//   //$("#yes").show(); //$("#yes").style.display = "block";
-//   //set #yes to visible?
-//   if (y < text.length) {
-//     $(id).append(text.charAt(i));
-//     y++;
-//     setTimeout(typeYes, speed);
-//   }
-// };
+function refresh()
+{
+  clearInterval(si);
+  i = 0;
+}
